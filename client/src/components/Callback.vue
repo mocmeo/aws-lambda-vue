487390ba602c943a79813b27f2f168c82335f0b0
mocmeo <vdev.lamnn@gmail.com>
2019-07-24 21:43:28 +0700

    integrating the Vue.js client with Auth0

---
+++ client/src/components/Callback.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>Loading your profile...</p>
+</template>
+
+<script>
+import auth0Client from "../AuthService";
+export default {
+  name: "Callback",
+  async created() {
+    await auth0Client.handleAuthentication();
+    this.$router.push("/");
+  }
+};
+</script>
+<style scoped>
+div > p {
+  margin: 0 auto;
+  text-align: center;
+  font-size: 22px;
+}
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,22 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import Callback from "@/components/Callback";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+  mode: "history",
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
+      path: "/",
+      name: "HelloWorld",
       component: HelloWorld
+    },
+    {
+      path: "/callback",
+      name: "Callback",
+      component: Callback
     }
   ]
-})
+});
